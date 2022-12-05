6654369b14bfe2e3f97634a8962c5b0211414cfe
Alexandre Lemarbre-Barrett <alexandre.lemarbre@gmail.com>
2022-12-05 13:40:57 -0500

    using vs2022 tools

--- vscode_startup.bat
+++ vscode_startup.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" && "C:\Program Files\Microsoft VS Code\Code.exe" .
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" && "C:\Program Files\Microsoft VS Code\Code.exe" .
