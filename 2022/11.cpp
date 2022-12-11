2db7a2f35c9a2e390d8c5af0ce7655014baf5ba3
Alexandre Lemarbre-Barrett <alexandre.lemarbre@gmail.com>
2022-12-11 11:51:12 -0500

    progress on 2022/11

--- 2022/11.cpp
+++ 2022/11.cpp
@@ -1,14 +1,71 @@
 #include "../helpers.h"
 #include "../common.h"
 
+#include <regex>
+#include <functional>
+
 using namespace std;
 
 // puzzle: https://adventofcode.com/2022/day/11
+
+class Monkey {
+    int id;
+    vector<int> items;
+    function<void(int&)> operation;
+    int divisibleBy;
+    int monkeyIfTrue;
+    int monkeyIfFalse;
+public:
+    Monkey(istream& is) {
+        string s, statement;
+        getline(is, s);
+
+        while (s != "") {
+            statement += s;
+            getline(is, s);
+        }
+
+        regex rgx("Monkey ([0-9]):  Starting items: ((?:[0-9]+,? ?)+)  Operation: new = old ([*-+/]) ([0-9]+)  Test: divisible by ([0-9]+)    If true: throw to monkey ([0-9]+)    If false: throw to monkey ([0-9]+)");
+        smatch matches;
+
+        if (regex_search(statement, matches, rgx)) {
+            id = stoi(matches[1].str());
+
+            string itemCSV = matches[2].str();
+
+            char op = matches[3].str()[0];
+            int value = stoi(matches[4].str());
+            operation = [this, op, value](int& item) {
+                switch(op) {
+                    case '*': item *= value; break;
+                    case '-': item -= value; break;
+                    case '+': item += value; break;
+                    case '/': item /= value; break;
+                }
+            };
+
+            divisibleBy = stoi(matches[5].str());
+            monkeyIfTrue = stoi(matches[6].str());
+            monkeyIfFalse = stoi(matches[7].str());
+        }
+    }
+};
+
+class MonkeyCircle {
+    vector<Monkey> monkeys;
+public:
+    MonkeyCircle(istream& is) {
+        while (is) {
+            monkeys.emplace_back(is);
+        }
+    }
+};
 
 int main() {
     FileParser fp("2022/11-data-test");
 
     // Parse file
+    auto mc = fp.parseOne<MonkeyCircle>();
 
     DayPartHandler<ostream> dph;
 
